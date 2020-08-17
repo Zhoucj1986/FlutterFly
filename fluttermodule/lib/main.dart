30649c1c5840834b4ed54e873473ff37dfecc85b
Zhoucj1986 <zcj-conan@163.com>
2020-08-17 17:01:28 +0800

    添加 flutter 插件

--- fluttermodule/lib/main.dart
+++ fluttermodule/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutterplugin/flutterplugin.dart';
 
 void main() => runApp(MyApp());
 
@@ -43,7 +46,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _platformVersion = 'Unknown';
   int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await Flutterplugin.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -97,6 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            Text('Running on: $_platformVersion\n'),
           ],
         ),
       ),
