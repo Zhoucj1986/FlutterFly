2a0d118375828a4b917f1e0c253e31e0772cfd7f
Zhoucj1986 <zcj-conan@163.com>
2020-08-20 18:00:08 +0800

    初步集成 flutter_boost

--- app/src/main/java/org/zhoucj/flutterfly/MainApplication.java
+++ app/src/main/java/org/zhoucj/flutterfly/MainApplication.java
@@ -1,13 +1,21 @@
 package org.zhoucj.flutterfly;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.pm.PackageInfo;
+import android.os.Build;
 
+import com.idlefish.flutterboost.FlutterBoost;
+import com.idlefish.flutterboost.Platform;
+import com.idlefish.flutterboost.Utils;
+import com.idlefish.flutterboost.interfaces.INativeRouter;
 import com.zhoucj.flutterfly.flutterplugin.FlutterpluginService;
 
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.embedding.engine.FlutterEngineCache;
-import io.flutter.embedding.engine.dart.DartExecutor;
+import java.util.Map;
+
+import io.flutter.embedding.android.FlutterView;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.StandardMessageCodec;
 
 
 /**
@@ -16,17 +24,85 @@ import io.flutter.embedding.engine.dart.DartExecutor;
  */
 public class MainApplication extends Application {
 
+
     @Override
     public void onCreate() {
         super.onCreate();
-        // Instantiate a FlutterEngine.
-        FlutterEngine flutterEngine = new FlutterEngine(this);
-        // Configure an initial route.
-        flutterEngine.getNavigationChannel().setInitialRoute("/");
-        // Start executing Dart code to pre-warm the FlutterEngine.
-        flutterEngine.getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
-        // Cache the FlutterEngine to be used by FlutterActivity or FlutterFragment.
-        FlutterEngineCache.getInstance().put("my_engine_id", flutterEngine);
+
+        INativeRouter router =new INativeRouter() {
+            @Override
+            public void openContainer(Context context, String url, Map<String, Object> urlParams, int requestCode, Map<String, Object> exts) {
+                String  assembleUrl= Utils.assembleUrl(url,urlParams);
+                PageRouter.openPageByUrl(context,assembleUrl, urlParams);
+            }
+
+        };
+
+        FlutterBoost.BoostLifecycleListener boostLifecycleListener= new FlutterBoost.BoostLifecycleListener(){
+
+            @Override
+            public void beforeCreateEngine() {
+
+            }
+
+            @Override
+            public void onEngineCreated() {
+
+                // 注册MethodChannel，监听flutter侧的getPlatformVersion调用
+                MethodChannel methodChannel = new MethodChannel(FlutterBoost.instance().engineProvider().getDartExecutor(), "flutter_native_channel");
+                methodChannel.setMethodCallHandler((call, result) -> {
+
+                    if (call.method.equals("getPlatformVersion")) {
+                        result.success(Build.VERSION.RELEASE);
+                    } else {
+                        result.notImplemented();
+                    }
+
+                });
+
+                // 注册PlatformView viewTypeId要和flutter中的viewType对应
+                FlutterBoost
+                        .instance()
+                        .engineProvider()
+                        .getPlatformViewsController()
+                        .getRegistry()
+                        .registerViewFactory("plugins.test/view", new TextPlatformViewFactory(StandardMessageCodec.INSTANCE));
+
+            }
+
+            @Override
+            public void onPluginsRegistered() {
+
+            }
+
+            @Override
+            public void onEngineDestroy() {
+
+            }
+
+        };
+
+        //
+        // AndroidManifest.xml 中必须要添加 flutterEmbedding 版本设置
+        //
+        //   <meta-data android:name="flutterEmbedding"
+        //               android:value="2">
+        //    </meta-data>
+        // GeneratedPluginRegistrant 会自动生成 新的插件方式　
+        //
+        // 插件注册方式请使用
+        // FlutterBoost.instance().engineProvider().getPlugins().add(new FlutterPlugin());
+        // GeneratedPluginRegistrant.registerWith()，是在engine 创建后马上执行，放射形式调用
+        //
+
+        Platform platform= new FlutterBoost
+                .ConfigBuilder(this,router)
+                .isDebug(true)
+                .whenEngineStart(FlutterBoost.ConfigBuilder.ANY_ACTIVITY_CREATED)
+                .renderMode(FlutterView.RenderMode.texture)
+                .lifecycleListener(boostLifecycleListener)
+                .build();
+        FlutterBoost.instance().init(platform);
 
         try {
             PackageInfo pi = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
